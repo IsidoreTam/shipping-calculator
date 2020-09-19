8e1b805201facfe24ea0e81e389fe2204404698d
IsidoreTam <isseygtam@gmail.com>
2020-09-19 14:10:36 -0600

    commit

--- js/index.js
+++ js/index.js
@@ -1,4 +1,8 @@
-let test = document.querySelector('input');
-console.log(typeof test);
-console.log(test);
-shippingrateDisplay.textContent = test;
+const calculateButton = document.querySelector('label')
+const shippingWeight = document.querySelector('input')
+
+calculateButton.addEventListener('click', function(){
+    let weight = shippingWeight.value
+    weight = Number(weight)
+    console.log(weight)
+})
